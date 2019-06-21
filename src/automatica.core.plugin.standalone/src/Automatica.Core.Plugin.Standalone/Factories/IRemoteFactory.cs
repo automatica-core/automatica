7a1f80d249e4826490c598af91eacc15445480bb
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2019-06-21 19:12:37 +0200

    added subtree code for automatica.core.plugin

---
+++ src/automatica.core.plugin.standalone/src/Automatica.Core.Plugin.Standalone/Factories/IRemoteFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using MQTTnet.Client;
+
+namespace Automatica.Core.Plugin.Standalone.Factories
+{
+    public interface IRemoteFactory
+    {
+        Task SubmitFactoryData(Guid factoryGuid, IMqttClient client);
+    }
+}
