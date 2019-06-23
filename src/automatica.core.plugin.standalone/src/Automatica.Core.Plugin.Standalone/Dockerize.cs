724407f481e3d41035631daf609b52f2d29fc82d
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2019-06-23 11:06:47 +0200

    changed assembly loading

--- src/automatica.core.plugin.standalone/src/Automatica.Core.Plugin.Standalone/Dockerize.cs
+++ src/automatica.core.plugin.standalone/src/Automatica.Core.Plugin.Standalone/Dockerize.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.Loader;
 using System.Threading.Tasks;
 using Automatica.Core.Base.Localization;
 using Automatica.Core.Base.Templates;
 using Automatica.Core.Driver;
 using Automatica.Core.Plugin.Standalone.Factories;
+using Microsoft.Extensions.DependencyModel;
 using Microsoft.Extensions.Logging;
 using MQTTnet.Client;
 
@@ -31,12 +33,41 @@ namespace Automatica.Core.Plugin.Standalone
 
         internal static Task<IList<T>> Init<T>(string workingDir, ILogger logger, ILocalizationProvider localizationProvider)
         {
+
             IList<T> retT = new List<T>();
             foreach (var file in Directory.GetFiles(workingDir, "*.dll"))
             {
+                Assembly AssemblyLoader(AssemblyLoadContext context, AssemblyName name)
+                {
+                    logger.LogDebug($"Try to load assembly {name} for {file}");
+                    // avoid loading *.resources dlls, because of: https://github.com/dotnet/coreclr/issues/8416
+                    if (name.Name.EndsWith("resources"))
+                    {
+                        return null;
+                    }
+
+                    logger.LogDebug($"try to load assembly from {workingDir}");
+                    var foundDlls = Directory.GetFileSystemEntries(workingDir, name.Name + ".dll", SearchOption.AllDirectories);
+                    if (foundDlls.Any())
+                    {
+                        return context.LoadFromAssemblyPath(foundDlls[0]);
+                    }
+
+                    var secondPath = new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName;
+                    logger.LogDebug($"try to load assembly from second path: {secondPath}");
+                    foundDlls = Directory.GetFileSystemEntries(secondPath, name.Name + ".dll", SearchOption.AllDirectories);
+                    if (foundDlls.Any())
+                    {
+                        return context.LoadFromAssemblyPath(foundDlls[0]);
+                    }
+
+                    return context.LoadFromAssemblyName(name);
+                }
+
                 Assembly assembly;
                 try
                 {
+                    AssemblyLoadContext.Default.Resolving += AssemblyLoader;
                     assembly = Assembly.LoadFile(file);
                 }
                 catch (Exception e)
@@ -46,7 +77,7 @@ namespace Automatica.Core.Plugin.Standalone
                 }
                 finally
                 {
-                    //AssemblyLoadContext.Default.Resolving -= assemblyLoader;
+                    AssemblyLoadContext.Default.Resolving -= AssemblyLoader;
                 }
 
                 var resources = assembly.GetManifestResourceNames().SingleOrDefault(a => a.EndsWith("automatica-manifest.json"));
@@ -76,5 +107,6 @@ namespace Automatica.Core.Plugin.Standalone
 
             return Task.FromResult(retT);
         }
+
     }
 }
