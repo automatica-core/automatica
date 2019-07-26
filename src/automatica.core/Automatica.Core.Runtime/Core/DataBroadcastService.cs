d45d2b9d41335edfa5a44360a317936743604c5b
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2019-07-26 14:35:12 +0200

    removed runtime ref from test-base

---
+++ src/automatica.core/Automatica.Core.Runtime/Core/DataBroadcastService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Automatica.Core.Base.IO;
+using Automatica.Push.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Automatica.Core.Runtime.Core
+{
+    internal class DataBroadcastService : IDataBroadcast
+    {
+        private readonly IHubContext<DataHub> _dataHub;
+
+        public DataBroadcastService(IHubContext<DataHub> dataHub)
+        {
+            _dataHub = dataHub;
+        }
+        public async Task DispatchValue(DispatchableType type, Guid id, object value)
+        {
+            await _dataHub.Clients.Group(id.ToString()).SendAsync("dispatchValue", type, id, value);
+        }
+    }
+}
---
+++ src/automatica.core/Automatica.Core.Runtime/Core/MqttPublishService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Automatica.Core.Base.IO;
+using Automatica.Core.Base.Remote;
+using Automatica.Core.Base.Serialization;
+using MQTTnet;
+using MQTTnet.Protocol;
+using MQTTnet.Server;
+
+namespace Automatica.Core.Runtime.Core
+{
+    internal class MqttPublishService : IRemoteSender
+    {
+        private readonly IMqttServer _mqttServer;
+
+        public MqttPublishService(IMqttServer mqttServer)
+        {
+            _mqttServer = mqttServer;
+        }
+
+        public async Task DispatchValue(IDispatchable dispatchable, object value)
+        {
+            var topic = $"{RemoteTopicConstants.DISPATCHER_TOPIC}/{dispatchable.Type}/{dispatchable.Id}";
+            await _mqttServer.PublishAsync(new MqttApplicationMessage()
+            {
+                Topic = topic,
+                QualityOfServiceLevel = MqttQualityOfServiceLevel.AtLeastOnce,
+                Payload = BinarySerializer.Serialize(value),
+                Retain = true
+            });
+        }
+    }
+}
