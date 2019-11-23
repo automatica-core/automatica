8a47f53829708a420db821b97562a675e552cf07
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2019-11-23 21:09:32 +0100

    fix unit tests

--- src/automatica.core/Automatica.Core.Common.Update/PluginManifest.cs
+++ src/automatica.core/Automatica.Core.Common.Update/PluginManifest.cs
@@ -1,9 +1,39 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using Automatica.Core.EF.Models;
+using Newtonsoft.Json.Converters;
 
 namespace Automatica.Core.Common.Update
 {
+    public class PluginVersion 
+    {
+        public int Minor { get; set; }
+        public int Major { get; set; }
+        public int Build { get; set; }
+        public int Revision { get; set; }
+
+        public Version ToVersion()
+        {
+            return new Version(Major, Minor, Build, Revision);
+        }
+
+        public static PluginVersion FromVersion(Version version)
+        {
+            if (version == null)
+            {
+                return null;
+            }
+            return new PluginVersion()
+            {
+                Major = version.Major,
+                Minor = version.Minor,
+                Build = version.Build,
+                Revision = version.Revision
+            };
+        }
+    }
+
     public class PluginManfiestData
     {
         public PluginManfiestData()
@@ -16,9 +46,23 @@ namespace Automatica.Core.Common.Update
         }
 
         [JsonProperty("manifest-version")]
+        public PluginVersion InternManifestVersion
+        {
+            get => Common.Update.PluginVersion.FromVersion(ManifestVersion);
+            set => ManifestVersion = value.ToVersion();
+        }
+
+        [JsonIgnore]
         public Version ManifestVersion { get; set; }
 
         [JsonProperty("plugin-version")]
+        public PluginVersion InternPluginVersion
+        {
+            get => Common.Update.PluginVersion.FromVersion(PluginVersion);
+            set => PluginVersion = value.ToVersion();
+        }
+
+        [JsonIgnore]
         public Version PluginVersion { get; set; }
 
         public Guid PluginGuid { get; set; }
@@ -36,6 +80,13 @@ namespace Automatica.Core.Common.Update
         public string ProjectPage { get; set; }
 
         [JsonProperty("MinCoreServerVersion")]
+        public PluginVersion InternMinCoreServerVersion
+        {
+            get => Common.Update.PluginVersion.FromVersion(MinCoreServerVersion);
+            set => MinCoreServerVersion = value.ToVersion();
+        }
+
+        [JsonIgnore]
         public Version MinCoreServerVersion { get; set; }
 
         public List<string> MandatoryLicenseFeatures { get; set; }
