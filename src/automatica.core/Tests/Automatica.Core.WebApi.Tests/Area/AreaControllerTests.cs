71eea60dbcc1caa2a176878547df7f6b791eabf6
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2020-01-28 18:11:16 +0100

    add create structure test for area

--- src/automatica.core/Tests/Automatica.Core.WebApi.Tests/Area/AreaControllerTests.cs
+++ src/automatica.core/Tests/Automatica.Core.WebApi.Tests/Area/AreaControllerTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Automatica.Core.WebApi.Controllers;
 using Automatica.Core.WebApi.Tests.Base;
@@ -31,10 +32,37 @@ namespace Automatica.Core.WebApi.Tests.Area
 
             formFileMoq.Setup(a => a.CopyTo(It.IsAny<Stream>())).Callback((Stream s) =>
             {
-                // impl copy to
+                using var proj = File.Open(Path.Combine("Area", "ETS5_Simple_ThreeLevel.knxproj"), FileMode.Open);
+                proj.CopyTo(s);
             });
+            formFileMoq.SetupGet(a => a.FileName).Returns("Upload.knxproj");
 
-           // await Controller.ProcessFile(null, formFileMoq.Object);
+            var structure = (await Controller.ProcessFile(Controller.GetInstances().First(), formFileMoq.Object)).ToList();
+
+            Assert.NotNull(structure);
+            Assert.NotEmpty(structure);
+
+            Assert.Equal("Project", structure.First().Name);
+
+            var demoStructure = structure.First().InverseThis2ParentNavigation.First();
+            Assert.Equal("DemoStructure", demoStructure.Name);
+
+            var building1 = demoStructure.InverseThis2ParentNavigation.FirstOrDefault(a => a.Name == "Building 1");
+            Assert.NotNull(building1);
+            Assert.Equal(2, building1.InverseThis2ParentNavigation.Count);
+
+            var firstFloor = building1.InverseThis2ParentNavigation.FirstOrDefault(a => a.Name == "First");
+            var groundFloor = building1.InverseThis2ParentNavigation.FirstOrDefault(a => a.Name == "Ground");
+
+            Assert.NotNull(firstFloor);
+            Assert.NotNull(groundFloor);
+
+            Assert.Equal("Sleep", firstFloor.InverseThis2ParentNavigation.First().Name);
+            Assert.Equal("Living", groundFloor.InverseThis2ParentNavigation.First().Name);
+
+            var garage = demoStructure.InverseThis2ParentNavigation.FirstOrDefault(a => a.Name == "Garage");
+            Assert.NotNull(garage);
+            Assert.Empty(garage.InverseThis2ParentNavigation);
         }
     }
 }
