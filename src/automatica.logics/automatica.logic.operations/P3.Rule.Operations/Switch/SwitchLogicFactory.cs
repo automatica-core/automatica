2140e3f14279c8eec605f61ce66cdab1d866fc45
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2020-01-31 18:46:58 +0100

    refactor codebase for new visualization

--- src/automatica.logics/automatica.logic.operations/P3.Rule.Operations/Switch/SwitchLogicFactory.cs
+++ src/automatica.logics/automatica.logic.operations/P3.Rule.Operations/Switch/SwitchLogicFactory.cs
@@ -2,7 +2,6 @@
 using Automatica.Core.Base.Templates;
 using Automatica.Core.EF.Models;
 using Automatica.Core.Rule;
-using Newtonsoft.Json;
 using RuleInterfaceDirection = Automatica.Core.Base.Templates.RuleInterfaceDirection;
 
 namespace P3.Rule.Operations.Switch
@@ -12,7 +11,6 @@ namespace P3.Rule.Operations.Switch
         public static Guid RuleInput = new Guid("a9fc6cad-d76f-464f-8b8a-a9873c695cb1");
         public static Guid RuleState = new Guid("b198329b-453a-4a2d-9f32-08a3287fb0f6");
         public static Guid RuleOutput = new Guid("ca8747ac-9c1d-4ca5-a91f-dd69d940bcb6");
-        public static Guid RuleTimerParameter = new Guid("4809f06b-bd0e-4723-baef-6a89790716e7");
 
         public override string RuleName => "P3.Rule.Operations.Switch";
         public override Guid RuleGuid => new Guid("3721acc3-5639-4cd2-aa30-a1952690d177");
@@ -24,12 +22,11 @@ namespace P3.Rule.Operations.Switch
             factory.CreateRuleTemplate(RuleGuid, "OPERATIONS.SWITCH.NAME", "OPERATIONS.SWITCH.DESCRIPTION",
                 "operations-switch", "OPERATIONS.NAME", 100, 100);
 
-            factory.CreateRuleInterfaceTemplate(RuleInput, "OPERATIONS.SWITCH.INPUT.NAME", "OPERATIONS.SWITCH.INPUT.DESCRIPTION", RuleGuid, RuleInterfaceDirection.Input, 0, 1);
-            factory.CreateRuleInterfaceTemplate(RuleState, "OPERATIONS.SWITCH.STATE.NAME", "OPERATIONS.SWITCH.STATE.DESCRIPTION", RuleGuid, RuleInterfaceDirection.Input, 0, 2);
+            factory.CreateRuleInterfaceTemplate(RuleInput, "OPERATIONS.SWITCH.INPUT.NAME", "OPERATIONS.SWITCH.INPUT.DESCRIPTION", RuleGuid, RuleInterfaceDirection.Input, 0, 1, RuleInterfaceType.Input);
+            factory.CreateRuleInterfaceTemplate(RuleState, "OPERATIONS.SWITCH.STATE.NAME", "OPERATIONS.SWITCH.STATE.DESCRIPTION", RuleGuid, RuleInterfaceDirection.Input, 0, 2, RuleInterfaceType.Status);
 
-            factory.CreateRuleInterfaceTemplate(RuleOutput, "OPERATIONS.SWITCH.OUTPUT.NAME", "OPERATIONS.SWITCH.OUTPUT.DESCRIPTION", RuleGuid, RuleInterfaceDirection.Output, 0, 1);
+            factory.CreateRuleInterfaceTemplate(RuleOutput, "OPERATIONS.SWITCH.OUTPUT.NAME", "OPERATIONS.SWITCH.OUTPUT.DESCRIPTION", RuleGuid, RuleInterfaceDirection.Output, 0, 1, RuleInterfaceType.Output);
             
-
             factory.ChangeDefaultVisuTemplate(RuleGuid, VisuMobileObjectTemplateTypes.ToggleButton);
         }
 
