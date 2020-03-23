e813cf47e7011a1c3e60d633c1fa97a5e618539b
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2020-03-23 14:03:33 +0100

    add dimmer logic factory

---
+++ src/automatica.logics/automatica.logic.operations/P3.Rule.Operations/Dimmer/DimmerLogic.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Automatica.Core.Base.IO;
+using Automatica.Core.EF.Models;
+using Automatica.Core.Rule;
+
+namespace P3.Rule.Operations.Dimmer
+{
+    public class DimmerLogic : Automatica.Core.Rule.Rule
+    {
+        private readonly RuleInterfaceInstance _input;
+        private readonly RuleInterfaceInstance _output;
+
+        public DimmerLogic(IRuleContext context) : base(context)
+        {
+            _input = context.RuleInstance.RuleInterfaceInstance.Single(a =>
+                a.This2RuleInterfaceTemplate == SwitchLogicFactory.RuleInput);
+            
+            _output = context.RuleInstance.RuleInterfaceInstance.Single(a =>
+                a.This2RuleInterfaceTemplate == SwitchLogicFactory.RuleOutput);
+        }
+
+        protected override IList<IRuleOutputChanged> InputValueChanged(RuleInterfaceInstance instance,
+            IDispatchable source, object value)
+        {
+            if (instance == _input)
+            {
+                return ValueChanged(_output, value);
+            }
+
+            return new List<IRuleOutputChanged>();
+        }
+
+        public override object GetDataForVisu()
+        {
+            return base.GetDataForVisu();
+        }
+    }
+}
