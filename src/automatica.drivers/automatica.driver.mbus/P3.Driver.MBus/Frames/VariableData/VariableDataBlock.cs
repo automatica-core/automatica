c111a7a1c084acdbc967bd465a7e7415b91d8783
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2021-01-24 11:43:19 +0100

    fix int24 to int32 mbus convert

--- src/automatica.drivers/automatica.driver.mbus/P3.Driver.MBus/Frames/VariableData/VariableDataBlock.cs
+++ src/automatica.drivers/automatica.driver.mbus/P3.Driver.MBus/Frames/VariableData/VariableDataBlock.cs
@@ -17,6 +17,8 @@ namespace P3.Driver.MBus.Frames.VariableData
 
         private byte[] _data;
 
+        public byte[] Data => _data;
+
         public int StorageNumber { get; private set; }
         public int Tariff { get; private set; }
 
@@ -192,20 +194,14 @@ namespace P3.Driver.MBus.Frames.VariableData
         }
         private static int Bit24ToInt32(byte[] byteArray)
         {
-            int result = (
-                ((0xFF & byteArray[0]) << 16) |
-                ((0xFF & byteArray[1]) << 8) |
-                (0xFF & byteArray[2])
-            );
-            if ((result & 0x00800000) > 0)
-            {
-                result = (int)((uint)result | (uint)0xFF000000);
-            }
-            else
-            {
-                result = (int)((uint)result & (uint)0x00FFFFFF);
-            }
-            return result;
+            var data = new byte[4];
+            data[0] = byteArray[0];
+            data[1] = byteArray[1];
+            data[2] = byteArray[2];
+
+            var test = BitConverter.ToInt32(data, 0);
+            
+            return test;
         }
         internal static long Bit48ToInt64(byte[] byteArray)
         {
