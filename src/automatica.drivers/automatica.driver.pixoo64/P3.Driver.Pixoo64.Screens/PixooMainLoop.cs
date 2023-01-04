5200fa85a99e1bb561eb9a4e219e58ff9dacc181
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2023-01-04 22:04:02 +0100

    update cancellation of main loop

--- src/automatica.drivers/automatica.driver.pixoo64/P3.Driver.Pixoo64.Screens/PixooMainLoop.cs
+++ src/automatica.drivers/automatica.driver.pixoo64/P3.Driver.Pixoo64.Screens/PixooMainLoop.cs
@@ -1,15 +1,19 @@
-﻿namespace P3.Driver.Pixoo64.Screens
+﻿using Microsoft.Extensions.Logging;
+
+namespace P3.Driver.Pixoo64.Screens
 {
     public class PixooMainLoop
     {
         private readonly PixooSharp.Pixoo64 _pixoo;
+        private readonly ILogger _logger;
         private readonly List<BaseScreen> _screens = new List<BaseScreen>();
 
         private CancellationTokenSource _cancellationTokenSource = new();
 
-        public PixooMainLoop(PixooSharp.Pixoo64 pixoo)
+        public PixooMainLoop(PixooSharp.Pixoo64 pixoo, ILogger logger)
         {
             _pixoo = pixoo;
+            _logger = logger;
         }
 
         public void AddScreen(BaseScreen screen)
@@ -24,25 +28,42 @@
 
             var task = Task.Run(function: async () =>
             {
-                int i = 0;
-                while (true)
+                try
                 {
-                    var screen = _screens[i];
-                    screen.Start();
-
-                    do
+                    int i = 0;
+                    while (true)
                     {
-                        await screen.Paint();
-                        Thread.Sleep(1000);
-                    } while (screen.TimeForNextScreen > 0);
+                        if (_cancellationTokenSource.IsCancellationRequested)
+                        {
+                            break;
+                        }
+
+                        var screen = _screens[i];
+                        screen.Start();
+
+                        do
+                        {
+                            if (_cancellationTokenSource.IsCancellationRequested)
+                            {
+                                break;
+                            }
+
+                            await screen.Paint();
+                            Thread.Sleep(1000);
+                        } while (screen.TimeForNextScreen > 0);
 
 
-                    i++;
+                        i++;
 
-                    if (i >= _screens.Count)
-                    {
-                        i = 0;
+                        if (i >= _screens.Count)
+                        {
+                            i = 0;
+                        }
                     }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, $"Exception occured {ex}");
                 }
             }, _cancellationTokenSource.Token).ConfigureAwait(false);
         }
