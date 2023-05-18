7ee69afb57e5c38f52d6baeecf38dd19217b0d2b
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2023-05-18 09:23:27 +0200

    fix drag&drop for nodeinstances

--- src/automatica.core/Automatica.WebNew/src/app/shared/config-tree/config-tree.component.ts
+++ src/automatica.core/Automatica.WebNew/src/app/shared/config-tree/config-tree.component.ts
@@ -405,15 +405,17 @@ export class ConfigTreeComponent extends BaseComponent implements OnInit, OnDest
       const parentDrag = this.nodeInstanceService.getNodeInstance(drag.ParentId);
       parentDrag.Children = parentDrag.Children.filter(a => a.ObjId != drag.ObjId);
 
-      drag.setParent(drop);
-      drop.Children = [...drop.Children, drag];
-      drag.This2ParentNodeInstance = drop.ObjId;
+      //drag.setParent(drop);
+      // const contains = drop.Children.filter(a => a.ObjId == drag.ObjId);
+      // if(contains.length == 0)
+       drop.Children = [...drop.Children, drag];
+       drag.This2ParentNodeInstance = drop.ObjId;
 
       this.selectNode(drag);
 
       this.appService.isLoading = true;
       this.tree.instance.refresh();
-      await this.configService.update(drop);
+      await this.configService.update(drag);
       this.appService.isLoading = false;
     }
   }
