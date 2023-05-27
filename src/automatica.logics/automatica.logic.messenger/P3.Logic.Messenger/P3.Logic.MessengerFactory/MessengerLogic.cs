83b635b8cbee081b918cae692497e844e3cc1afe
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2023-05-27 20:39:32 +0200

    add some logging for cloud messenger

--- src/automatica.logics/automatica.logic.messenger/P3.Logic.Messenger/P3.Logic.MessengerFactory/MessengerLogic.cs
+++ src/automatica.logics/automatica.logic.messenger/P3.Logic.Messenger/P3.Logic.MessengerFactory/MessengerLogic.cs
@@ -13,6 +13,8 @@ namespace P3.Logic.Messenger
         private readonly IList<string> _to = new List<string>();
         private readonly string _subject = "Automatica.Core Message";
 
+        private object _value;
+
         public MessengerLogic(IRuleContext context) : base(context)
         {
             var toProperty = context.RuleInstance.RuleInterfaceInstance.SingleOrDefault(a =>
@@ -36,20 +38,35 @@ namespace P3.Logic.Messenger
 
         protected override IList<IRuleOutputChanged> InputValueChanged(RuleInterfaceInstance instance, IDispatchable source, object value)
         {
-            if (_to.Count > 0)
+
+            if (_to.Count > 0 && value != _value)
             {
                 try
                 {
-                    Context.Logger.LogInformation($"Send email to {String.Join(";", _to)}");
-                    Context.CloudApi.SendEmail(_to, _subject,
-                        $"\"{Context.RuleInstance.Name}\" received value \"{value}\" from source \"{source.Name}\"");
+                    bool execute = false;
+                    if (value is bool bValue)
+                        execute = bValue;
+                    else
+                        execute = true;
+
+                    if (execute)
+                    {
+                        Context.Logger.LogInformation($"Send email to {String.Join(";", _to)}");
+                        Context.CloudApi.SendEmail(_to, _subject,
+                            $"\"{Context.RuleInstance.Name}\" received value \"{value}\" from source \"{source.Name}\"");
+                    }
+                    else
+                    {
+                        Context.Logger.LogInformation($"Ignore message, because value is either false or hasn't changed newValue {value} oldValue {_value}");
+                    }
                 }
                 catch (Exception e)
                 {
                     Context.Logger.LogError(e, $"Error sending email {e}");
                 }
             }
-            
+
+            _value = value;
             return base.InputValueChanged(instance, source, value);
         }
     }
