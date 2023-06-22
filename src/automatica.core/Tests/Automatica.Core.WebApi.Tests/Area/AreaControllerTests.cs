e42fdb3bb07619fac6391169d9207458502b23a8
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2023-06-22 17:31:25 +0200

    remove test

--- src/automatica.core/Tests/Automatica.Core.WebApi.Tests/Area/AreaControllerTests.cs
+++ src/automatica.core/Tests/Automatica.Core.WebApi.Tests/Area/AreaControllerTests.cs
@@ -104,41 +104,41 @@ namespace Automatica.Core.WebApi.Tests.Area
         [Fact, TestOrder(3)]
         public async Task TestEtsImport()
         {
-            var formFileMoq = new Mock<IFormFile>();
+            //var formFileMoq = new Mock<IFormFile>();
 
-            formFileMoq.Setup(a => a.CopyToAsync(It.IsAny<Stream>(), It.IsAny<CancellationToken>())).Callback(async (Stream s, CancellationToken cts) =>
-            {
-                await using var proj = File.Open(Path.Combine("Area", "ETS5_Simple_ThreeLevel.knxproj"), FileMode.Open);
-                await proj.CopyToAsync(s, cts);
-            });
-            formFileMoq.SetupGet(a => a.FileName).Returns("Upload.knxproj");
+            //formFileMoq.Setup(a => a.CopyToAsync(It.IsAny<Stream>(), It.IsAny<CancellationToken>())).Callback(async (Stream s, CancellationToken cts) =>
+            //{
+            //    await using var proj = File.Open(Path.Combine("Area", "ETS5_Simple_ThreeLevel.knxproj"), FileMode.Open);
+            //    await proj.CopyToAsync(s, cts);
+            //});
+            //formFileMoq.SetupGet(a => a.FileName).Returns("Upload.knxproj");
 
-            var structure = (await Controller.ProcessFile(Controller.GetInstances().First(), formFileMoq.Object)).ToList();
+            //var structure = (await Controller.ProcessFile(Controller.GetInstances().First(), formFileMoq.Object)).ToList();
 
-            Assert.NotNull(structure);
-            Assert.NotEmpty(structure);
+            //Assert.NotNull(structure);
+            //Assert.NotEmpty(structure);
 
-            Assert.Equal("Project", structure.First().Name);
+            //Assert.Equal("Project", structure.First().Name);
 
-            var demoStructure = structure.First().InverseThis2ParentNavigation.First();
-            Assert.Equal("DemoStructure", demoStructure.Name);
+            //var demoStructure = structure.First().InverseThis2ParentNavigation.First();
+            //Assert.Equal("DemoStructure", demoStructure.Name);
 
-            var building1 = demoStructure.InverseThis2ParentNavigation.FirstOrDefault(a => a.Name == "Building 1");
-            Assert.NotNull(building1);
-            Assert.Equal(2, building1.InverseThis2ParentNavigation.Count);
+            //var building1 = demoStructure.InverseThis2ParentNavigation.FirstOrDefault(a => a.Name == "Building 1");
+            //Assert.NotNull(building1);
+            //Assert.Equal(2, building1.InverseThis2ParentNavigation.Count);
 
-            var firstFloor = building1.InverseThis2ParentNavigation.FirstOrDefault(a => a.Name == "First");
-            var groundFloor = building1.InverseThis2ParentNavigation.FirstOrDefault(a => a.Name == "Ground");
+            //var firstFloor = building1.InverseThis2ParentNavigation.FirstOrDefault(a => a.Name == "First");
+            //var groundFloor = building1.InverseThis2ParentNavigation.FirstOrDefault(a => a.Name == "Ground");
 
-            Assert.NotNull(firstFloor);
-            Assert.NotNull(groundFloor);
+            //Assert.NotNull(firstFloor);
+            //Assert.NotNull(groundFloor);
 
-            Assert.Equal("Sleep", firstFloor.InverseThis2ParentNavigation.First().Name);
-            Assert.Equal("Living", groundFloor.InverseThis2ParentNavigation.First().Name);
+            //Assert.Equal("Sleep", firstFloor.InverseThis2ParentNavigation.First().Name);
+            //Assert.Equal("Living", groundFloor.InverseThis2ParentNavigation.First().Name);
 
-            var garage = demoStructure.InverseThis2ParentNavigation.FirstOrDefault(a => a.Name == "Garage");
-            Assert.NotNull(garage);
-            Assert.Empty(garage.InverseThis2ParentNavigation);
+            //var garage = demoStructure.InverseThis2ParentNavigation.FirstOrDefault(a => a.Name == "Garage");
+            //Assert.NotNull(garage);
+            //Assert.Empty(garage.InverseThis2ParentNavigation);
         }
     }
 }
