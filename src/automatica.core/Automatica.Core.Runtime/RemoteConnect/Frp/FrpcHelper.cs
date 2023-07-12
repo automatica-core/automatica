476354bd1075f4380ca0aebed6e311cbe453ad88
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2023-07-12 10:27:05 +0200

    fix create frp file

--- src/automatica.core/Automatica.Core.Runtime/RemoteConnect/Frp/FrpcHelper.cs
+++ src/automatica.core/Automatica.Core.Runtime/RemoteConnect/Frp/FrpcHelper.cs
@@ -3,6 +3,7 @@ using System;
 using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
+using Automatica.Core.Base.Common;
 using Automatica.Core.Base.Tunneling;
 
 namespace Automatica.Core.Runtime.RemoteConnect.Frp
@@ -11,46 +12,41 @@ namespace Automatica.Core.Runtime.RemoteConnect.Frp
     {
         private const string FrpcTemplateFile = "frpc_template.ini";
 
-        public static async Task CreateServiceFileFromTemplate(TunnelingProtocol tunnelingProtocol, string name, string address, int targetPort,
+        public static async Task CreateServiceFileFromTemplate(TunnelingProtocol tunnelingProtocol, string name,
+            string address, int targetPort,
             int remotePort, CancellationToken token)
         {
-            var currentDir = Directory.GetCurrentDirectory();
-            try
+            var currentDir = ServerInfo.GetBasePath();
+
+            if (!Directory.Exists(Path.Combine(currentDir, "frp")))
             {
-                if (!Directory.Exists("frp"))
-                {
-                    throw new ArgumentException("Could not find configuration directory!");
-                }
-
-                if (!File.Exists(Path.Combine("frp", FrpcTemplateFile)))
-                {
-                    throw new ArgumentException(
-                        $"Could not find config file '{FrpcTemplateFile}' in {Directory.GetCurrentDirectory()}");
-                }
-
-                var templateFile = await File.ReadAllLinesAsync(Path.Combine("frp", FrpcTemplateFile), token);
-                await using var newFile =
-                    new StreamWriter(Path.Combine("frp", "enabled", $"{name}_{tunnelingProtocol.ToString().ToLowerInvariant()}.ini"));
-
-                foreach (var line in templateFile)
-                {
-                    var newLine = line.Replace("{{name}}", name)
-                                            .Replace("{{type}}", $"{tunnelingProtocol.ToString().ToLowerInvariant()}")
-                                            .Replace("{{local_ip}}", address)
-                                            .Replace("{{local_port}}", $"{targetPort}")
-                                            .Replace("{{remote_port}}", $"{remotePort}");
-                    await newFile.WriteLineAsync(newLine);
-                }
-
-                newFile.Close();
+                throw new ArgumentException("Could not find configuration directory!");
             }
-            finally
+
+            if (!File.Exists(Path.Combine(currentDir, "frp", FrpcTemplateFile)))
             {
-
-                Directory.SetCurrentDirectory(currentDir);
+                throw new ArgumentException(
+                    $"Could not find config file '{FrpcTemplateFile}' in {Directory.GetCurrentDirectory()}");
             }
-            
+
+            var templateFile = await File.ReadAllLinesAsync(Path.Combine(currentDir, "frp", FrpcTemplateFile), token);
+            await using var newFile =
+                new StreamWriter(Path.Combine(currentDir, "frp", "enabled",
+                    $"{name}_{tunnelingProtocol.ToString().ToLowerInvariant()}.ini"));
+
+            foreach (var line in templateFile)
+            {
+                var newLine = line.Replace("{{name}}", name)
+                    .Replace("{{type}}", $"{tunnelingProtocol.ToString().ToLowerInvariant()}")
+                    .Replace("{{local_ip}}", address)
+                    .Replace("{{local_port}}", $"{targetPort}")
+                    .Replace("{{remote_port}}", $"{remotePort}");
+                await newFile.WriteLineAsync(newLine);
+            }
+
+            newFile.Close();
+
         }
-        
+
     }
 }
