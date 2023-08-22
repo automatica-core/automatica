d6949904e99bfc4fba8a94f9a4c4b8b6c785dcde
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2023-08-22 19:38:48 +0200

    add startup ctor again

--- src/automatica.core/Automatica.Core/Startup.cs
+++ src/automatica.core/Automatica.Core/Startup.cs
@@ -42,6 +42,11 @@ namespace Automatica.Core
        
         public IConfiguration Configuration { get; private set; }
 
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
