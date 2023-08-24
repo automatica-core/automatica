e36ee6b5c0558c6ba378dff3dfb95699bc728168
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2023-08-24 15:08:30 +0200

    update version

--- src/automatica.logics/automatica.sonos.control/P3.Logic.Sonos/SonosControl/SonosControlLogicFactory.cs
+++ src/automatica.logics/automatica.sonos.control/P3.Logic.Sonos/SonosControl/SonosControlLogicFactory.cs
@@ -7,7 +7,7 @@ namespace P3.Logic.Sonos.SonosControl;
 
 public class SonosControlLogicFactory : LogicFactory
 {
-    public override Version LogicVersion => new Version(0, 4, 0, 2);
+    public override Version LogicVersion => new Version(1, 0, 0, 0);
 
     public override bool InDevelopmentMode => true;
 
