e597ba9402530a4aa09c507e7dae3960cda9fd35
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2023-08-26 08:51:23 +0200

    update unit tests

--- src/automatica.logics/automatica.sonos.control/P3.Logic.Sonos.Test/SonosControl/SonosControlTests.cs
+++ src/automatica.logics/automatica.sonos.control/P3.Logic.Sonos.Test/SonosControl/SonosControlTests.cs
@@ -85,6 +85,78 @@ public class SonosControlTests : LogicTest<SonosControlLogicFactory>
 
     }
 
+    [Fact]
+    public async void Test_InitialPlayStatusIsFalse()
+    {
+        await Context.Dispatcher.ClearValues();
+        await Context.Dispatcher.ClearRegistrations();
+
+        LogicInputChanged(GetLogicInterfaceByTemplate(SonosControlLogicFactory.PlayDefaultTrigger), false);
+
+        await Task.Delay(500);
+
+        var values = Context.Dispatcher.GetValues(DispatchableType.RuleInstance);
+
+        Assert.Equal(0, values.Count);
+    }
+    [Fact]
+    public async void Test_InitialPlayStatusIsFalse2()
+    {
+        await Context.Dispatcher.ClearValues();
+        await Context.Dispatcher.ClearRegistrations();
+
+        LogicInputChanged(GetLogicInterfaceByTemplate(SonosControlLogicFactory.PlayPauseTrigger), false);
+
+        await Task.Delay(500);
+
+        var values = Context.Dispatcher.GetValues(DispatchableType.RuleInstance);
+
+        Assert.Equal(0, values.Count);
+    }
+    [Fact]
+    public async void Test_InitialPlayStatusIsFalseAndChanges()
+    {
+        await Context.Dispatcher.ClearValues();
+        await Context.Dispatcher.ClearRegistrations();
+
+        LogicInputChanged(GetLogicInterfaceByTemplate(SonosControlLogicFactory.PlayPauseTrigger), false);
+
+        await Task.Delay(500);
+
+        var values = Context.Dispatcher.GetValues(DispatchableType.RuleInstance);
+
+        Assert.Equal(0, values.Count);
+
+
+        LogicInputChanged(GetLogicInterfaceByTemplate(SonosControlLogicFactory.PlayDefaultTrigger), true);
+
+        await Task.Delay(500);
+
+        values = Context.Dispatcher.GetValues(DispatchableType.RuleInstance);
+
+        Assert.Equal(4, values.Count);
+
+        AssertInternal(values, SonosControlLogicFactory.PlayOutputStatus, true);
+        AssertInternal(values, SonosControlLogicFactory.VolumeOutputStatus, SonosControlLogicFactory.DefaultVolume);
+        AssertInternal(values, SonosControlLogicFactory.RadioStationOutputValue, SonosControlLogicFactory.DefaultRadioStation);
+        AssertInternal(values, SonosControlLogicFactory.PauseOutputStatus, false);
+
+
+        LogicInputChanged(GetLogicInterfaceByTemplate(SonosControlLogicFactory.VolumeIncrement), true);
+        values = Context.Dispatcher.GetValues(DispatchableType.RuleInstance);
+
+        AssertInternal(values, SonosControlLogicFactory.VolumeOutputStatus, SonosControlLogicFactory.DefaultVolume + 1);
+
+
+
+        LogicInputChanged(GetLogicInterfaceByTemplate(SonosControlLogicFactory.PlayPauseTrigger), false);
+        values = Context.Dispatcher.GetValues(DispatchableType.RuleInstance);
+
+
+        AssertInternal(values, SonosControlLogicFactory.PlayOutputStatus, false);
+        AssertInternal(values, SonosControlLogicFactory.PauseOutputStatus, true);
+    }
+
     private void AssertInternal(IDictionary<Guid, DispatchValue> values, Guid guid, object value)
     {
         Assert.Equal(value, values[guid].Value);
