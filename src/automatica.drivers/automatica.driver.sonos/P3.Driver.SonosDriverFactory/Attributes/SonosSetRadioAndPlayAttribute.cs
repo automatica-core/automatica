3769efead12a26de3223c942208ce88e565612cc
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2023-09-06 11:12:22 +0200

    fix nuget update

--- src/automatica.drivers/automatica.driver.sonos/P3.Driver.SonosDriverFactory/Attributes/SonosSetRadioAndPlayAttribute.cs
+++ src/automatica.drivers/automatica.driver.sonos/P3.Driver.SonosDriverFactory/Attributes/SonosSetRadioAndPlayAttribute.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Automatica.Core.Base.IO;
 using Automatica.Core.Driver;
@@ -13,12 +14,12 @@ namespace P3.Driver.SonosDriverFactory.Attributes
         {
         }
 
-        public override async Task WriteValue(IDispatchable source, object value)
+        protected override async Task Write(object value, IWriteContext writeContext, CancellationToken token = new CancellationToken())
         {
             try
             {
                 DriverContext.Logger.LogDebug($"Sonos play...");
-                await base.WriteValue(source, value);
+                await base.Write(value, writeContext, token);
                 await Device.Controller.PlayAsync();
             }
             catch (Exception e)
