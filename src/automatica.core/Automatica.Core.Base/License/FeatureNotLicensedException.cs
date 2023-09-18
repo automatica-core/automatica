4d2cbd73aec193be30a2ce6677e10965d6a706d4
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2023-09-18 16:49:59 +0200

    add exception for feature not licensed

---
+++ src/automatica.core/Automatica.Core.Base/License/FeatureNotLicensedException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Automatica.Core.Base.License
+{
+    public sealed class FeatureNotLicensedException : Exception
+    {
+        public string FeatureName { get; }
+
+        public FeatureNotLicensedException(string featureName)
+        {
+            FeatureName = featureName;
+        }
+    }
+}
