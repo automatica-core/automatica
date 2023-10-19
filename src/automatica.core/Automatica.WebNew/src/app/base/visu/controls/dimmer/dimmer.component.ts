3a95d2d08791752daf0eb24b0c00447732b5fa53
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2023-10-19 20:42:18 +0200

    remove unused usings/code

--- src/automatica.core/Automatica.WebNew/src/app/base/visu/controls/dimmer/dimmer.component.ts
+++ src/automatica.core/Automatica.WebNew/src/app/base/visu/controls/dimmer/dimmer.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { BaseMobileRuleComponent } from "../../base-mobile-rule-component";
-import { DataHubService } from "src/app/base/communication/hubs/data-hub.service";
-import { NotifyService } from "src/app/services/notify.service";
-import { L10nTranslationService } from "angular-l10n";
-import { ConfigService } from "src/app/services/config.service";
-import { LogicInstanceVisuService } from "src/app/services/logic-visu.service";
-import { AppService } from "src/app/services/app.service";
 import { RuleInterfaceType } from "src/app/base/model/rule-interface-template";
 import { RuleInterfaceInstance } from "src/app/base/model/rule-interface-instance";
 
--- src/automatica.core/Automatica.WebNew/src/app/base/visu/controls/shutter/shutter.component.ts
+++ src/automatica.core/Automatica.WebNew/src/app/base/visu/controls/shutter/shutter.component.ts
@@ -1,15 +1,6 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { BaseMobileRuleComponent } from "../../base-mobile-rule-component";
-import { DataHubService } from "src/app/base/communication/hubs/data-hub.service";
-import { NotifyService } from "src/app/services/notify.service";
-import { L10nTranslationService } from "angular-l10n";
-import { ConfigService } from "src/app/services/config.service";
-import { LogicInstanceVisuService } from "src/app/services/logic-visu.service";
-import { AppService } from "src/app/services/app.service";
-import { RuleInterfaceType } from "src/app/base/model/rule-interface-template";
 import { RuleInterfaceInstance } from "src/app/base/model/rule-interface-instance";
-import { ThemeService } from "src/app/services/theme.service";
-
 
 @Component({
   selector: "visu-shutter",
@@ -36,17 +27,6 @@ export class ShutterComponent extends BaseMobileRuleComponent implements OnInit,
   displayValue: string;
 
   shutterIcon: any;
-
-  constructor(
-    dataHubService: DataHubService,
-    notify: NotifyService,
-    translate: L10nTranslationService,
-    configService: ConfigService,
-    ruleInstanceVisuService: LogicInstanceVisuService,
-    appService: AppService) {
-    super(dataHubService, notify, translate, configService, ruleInstanceVisuService, appService);
-  }
-
 
   async ngOnInit() {
     this.baseOnInit();
