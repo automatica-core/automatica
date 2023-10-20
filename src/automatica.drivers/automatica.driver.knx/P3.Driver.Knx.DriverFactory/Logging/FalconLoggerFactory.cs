e3650b8b7b1eafae38de4d5f3ffcbca87f1c26ae
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2023-10-20 08:15:04 +0200

    add falcon logger and use async dispose method

---
+++ src/automatica.drivers/automatica.driver.knx/P3.Driver.Knx.DriverFactory/Logging/FalconLoggerFactory.cs
@@ -0,0 +1,20 @@
+﻿using Knx.Falcon.Logging;
+using Microsoft.Extensions.Logging;
+
+namespace P3.Driver.Knx.DriverFactory.Logging
+{
+    internal class FalconLoggerFactory : IFalconLoggerFactory
+    {
+        private readonly ILogger _logger;
+
+        public FalconLoggerFactory(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public IFalconLogger GetLogger(string name)
+        {
+            return new FalconLogger(_logger, name);
+        }
+    }
+}
