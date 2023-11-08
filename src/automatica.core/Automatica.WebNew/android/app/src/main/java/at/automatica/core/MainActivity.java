1073cfd022dadb4556ffb93e414e9d0356de5746
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2023-11-08 16:01:32 +0100

    ignore ssl errors

--- src/automatica.core/Automatica.WebNew/android/app/src/main/java/at/automatica/core/MainActivity.java
+++ src/automatica.core/Automatica.WebNew/android/app/src/main/java/at/automatica/core/MainActivity.java
@@ -1,5 +1,25 @@
 package at.automatica.core;
 
-import com.getcapacitor.BridgeActivity;
+import android.net.http.SslError;
+import android.os.Bundle;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebView;
 
-public class MainActivity extends BridgeActivity {}
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.BridgeWebViewClient;
+import com.getcapacitor.Plugin;
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    this.bridge.getWebView().setWebViewClient(new BridgeWebViewClient(this.bridge) {
+      @Override
+      public void onReceivedSslError(WebView view, final SslErrorHandler handler, SslError error) {
+        handler.proceed();
+      }
+    });
+
+  }
+}
