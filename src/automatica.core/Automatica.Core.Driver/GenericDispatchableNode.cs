decd0407fdb5977ffe46af515a039d565ac593b2
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2023-12-19 20:35:32 +0100

    add generic dispatchable object

---
+++ src/automatica.core/Automatica.Core.Driver/GenericDispatchableNode.cs
@@ -0,0 +1,24 @@
+﻿using Automatica.Core.Base.IO;
+using System;
+
+namespace Automatica.Core.Driver
+{
+    public sealed class GenericDispatchableNode : IDispatchable
+    {
+        public GenericDispatchableNode(string name, Guid id, DispatchableSource source)
+        {
+            Type = DispatchableType.NodeInstance;
+            Name = name;
+            Id = id;
+            Source = source;
+            IsRemanent = false;
+        }
+
+        public DispatchableType Type { get; }
+        public string Name { get; }
+        public Guid Id { get; }
+        public bool IsRemanent { get; }
+
+        public DispatchableSource Source { get; }
+    }
+}
