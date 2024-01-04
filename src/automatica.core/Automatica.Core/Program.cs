1a61b8d5c19afced3fb9feff25c13682abf27a5b
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2024-01-04 11:05:44 +0100

    set tz env

--- src/automatica.core/Automatica.Core/Program.cs
+++ src/automatica.core/Automatica.Core/Program.cs
@@ -50,6 +50,11 @@ namespace Automatica.Core
                 logger.LogError($"Could not find appsettings.json in {ServerInfo.GetConfigDirectory()}");
             }
 
+            var tz = config["TZ"];
+            if (!String.IsNullOrEmpty(tz)) {
+                Environment.SetEnvironmentVariable("TZ", tz);
+            }
+
             if (!String.IsNullOrEmpty(Environment.GetEnvironmentVariable("MQTT_LOG_VERBOSE")))
             {
                 //MqttNetGlobalLogger.LogMessagePublished += (s, e) =>
