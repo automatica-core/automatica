487ce6d88e4d3d2af5e8f03fad748f8b7dcff2c8
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2024-01-04 21:30:36 +0100

    add console logger for tests

--- src/automatica.core/Automatica.Core.UnitTests/Logics/LogicContextMock.cs
+++ src/automatica.core/Automatica.Core.UnitTests/Logics/LogicContextMock.cs
@@ -62,24 +62,6 @@ namespace Automatica.Core.UnitTests.Base.Logics
         public TimeProvider TimeProvider => FakeTimeProvider.Instance;
     }
 
-    public class ConsoleTestLogger : ILogger
-    {
-        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
-        {
-            Console.WriteLine(formatter(state, exception));
-        }
-
-        public bool IsEnabled(LogLevel logLevel)
-        {
-            return true;
-        }
-
-        public IDisposable BeginScope<TState>(TState state) where TState : notnull
-        {
-            throw new NotImplementedException();
-        }
-    }
-
     public class FakeTimeProvider : TimeProvider
     {
         private DateTime _dateTime = DateTime.Now;
