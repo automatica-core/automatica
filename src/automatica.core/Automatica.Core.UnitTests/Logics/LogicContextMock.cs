349e5330fa15588c754059ce3b3af4564e28e740
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2024-01-04 21:30:15 +0100

    add console logger for tests

--- src/automatica.core/Automatica.Core.UnitTests/Logics/LogicContextMock.cs
+++ src/automatica.core/Automatica.Core.UnitTests/Logics/LogicContextMock.cs
@@ -10,7 +10,6 @@ using Automatica.Core.EF.Models;
 using Automatica.Core.Logic;
 using Automatica.Core.UnitTests.Base.Common;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
 
 
@@ -43,6 +42,9 @@ namespace Automatica.Core.UnitTests.Base.Logics
             Dispatcher = dispatcher;
             Factory = factory;
             ControlContext = new Mock<IControlContext>().Object;
+
+
+            Logger = new ConsoleTestLogger();
         }
         public RuleInstance RuleInstance { get; set; }
         public IDispatcher Dispatcher { get; }
@@ -50,13 +52,32 @@ namespace Automatica.Core.UnitTests.Base.Logics
 
         public IRuleInstanceVisuNotify Notify { get; }
 
-        public ILogger Logger => NullLogger.Instance;
+        public ILogger Logger { get; }
+    
         public IServerCloudApi CloudApi => new CloudApiMock();
         public ILicenseContract LicenseContract => new LicenseContractMock();
 
         public IControlContext ControlContext { get; }
 
         public TimeProvider TimeProvider => FakeTimeProvider.Instance;
+    }
+
+    public class ConsoleTestLogger : ILogger
+    {
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            Console.WriteLine(formatter(state, exception));
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public IDisposable BeginScope<TState>(TState state) where TState : notnull
+        {
+            throw new NotImplementedException();
+        }
     }
 
     public class FakeTimeProvider : TimeProvider
