efcc9686474c6e4c8a0d47509b50403f25fbfce5
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2024-01-15 14:37:32 +0100

    extend IBlind interface

--- src/automatica.core/Automatica.Core.Control/Base/IBlind.cs
+++ src/automatica.core/Automatica.Core.Control/Base/IBlind.cs
@@ -1,7 +1,11 @@
 ﻿namespace Automatica.Core.Control.Base
 {
-    public interface IBlind
+    public interface IBlind : IControl, IControlValueCallback
     {
+        bool IsMoving { get; }
+        int Direction { get; }
+        int Position { get; }
+
         Task StopAsync(CancellationToken token = default);
         Task MoveUpAsync(CancellationToken token = default);
         Task MoveDownAsync(CancellationToken token = default);
