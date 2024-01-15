0c36449cf063eff912cb9defe95d37f87b42d067
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2024-01-15 14:17:45 +0100

    implement IBlind Interface

--- src/automatica.logics/automatica.logic.operations/P3.Logic.Operations/Shutter/ShutterLogic.cs
+++ src/automatica.logics/automatica.logic.operations/P3.Logic.Operations/Shutter/ShutterLogic.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Automatica.Core.Base.IO;
+using Automatica.Core.Control.Base;
 using Automatica.Core.EF.Models;
 using Automatica.Core.Logic;
-using Newtonsoft.Json.Linq;
 
 namespace P3.Logic.Operations.Shutter
 {
-    public class ShutterLogic: Automatica.Core.Logic.Logic
+    public class ShutterLogic: Automatica.Core.Logic.Logic, IBlind
     {
 
         private readonly RuleInterfaceInstance _moveInput;
@@ -71,40 +73,19 @@ namespace P3.Logic.Operations.Shutter
             {
                 if (!_locked)
                 {
-                    if (!_moving || _direction != 1)
-                    {
-                        ret.Add(new LogicOutputChanged(_moveOutput, 0));
-                        ret.Add(new LogicOutputChanged(_isMovingOutput, true));
-                    }
-
-                    _direction = 1;
-                    _moving = true;
+                    ret.AddRange(MoveUp());
                 }
             }
             else if (instance.ObjId == _downInput.ObjId)
             {
                 if (!_locked)
                 {
-                    if (!_moving || _direction != 0)
-                    {
-                        ret.Add(new LogicOutputChanged(_moveOutput, 1));
-                        ret.Add(new LogicOutputChanged(_isMovingOutput, true));
-                    }
-
-                    _moving = true;
-                    _direction = 0;
+                    ret.AddRange(MoveDown());
                 }
             }
             else if (instance.ObjId == _stopInput.ObjId)
             {
-                if (_moving)
-                {
-                    var stopValue = _direction == 0 ? true : false;
-                    ret.Add(new LogicOutputChanged(_stopOutput, stopValue));
-                    ret.Add(new LogicOutputChanged(_isMovingOutput, false));
-                }
-
-                _moving = false;
+                ret.AddRange(Stop());
             }
             else if (instance.ObjId == _lockedInput.ObjId)
             {
@@ -124,27 +105,107 @@ namespace P3.Logic.Operations.Shutter
             else if (instance.ObjId == _absolutePositionInput.ObjId)
             {
                 var dValue = Convert.ToDouble(value);
-
-                if (Math.Ceiling(dValue) >= 100)
-                {
-                    _moving = false;
-                    ret.Add(new LogicOutputChanged(_isMovingOutput, false));
-                }
-                else if (Math.Floor(dValue) <= 0)
-                {
-                    _moving = false;
-                    ret.Add(new LogicOutputChanged(_isMovingOutput, false));
-                }
-                _position = dValue;
-                ret.Add(new LogicOutputChanged(_absolutePositionOutput, dValue));
+                ret.AddRange(MoveAbsolute(dValue));
             }
 
             return ret;
         }
 
-        public override object GetDataForVisu()
+        private IList<ILogicOutputChanged> MoveAbsolute(double dValue)
         {
-            return base.GetDataForVisu();
+            var ret = new List<ILogicOutputChanged>();
+
+            if (Math.Ceiling(dValue) >= 100)
+            {
+                _moving = false;
+                ret.Add(new LogicOutputChanged(_isMovingOutput, false));
+            }
+            else if (Math.Floor(dValue) <= 0)
+            {
+                _moving = false;
+                ret.Add(new LogicOutputChanged(_isMovingOutput, false));
+            }
+            _position = dValue;
+            ret.Add(new LogicOutputChanged(_absolutePositionOutput, dValue));
+
+            return ret;
+        }
+
+        private IList<ILogicOutputChanged> MoveUp()
+        {
+            var ret = new List<ILogicOutputChanged>();
+            if (!_moving || _direction != 1)
+            {
+                ret.Add(new LogicOutputChanged(_moveOutput, 0));
+                ret.Add(new LogicOutputChanged(_isMovingOutput, true));
+            }
+
+            _direction = 1;
+            _moving = true;
+            return ret;
+        }
+
+        private IList<ILogicOutputChanged> MoveDown()
+        {
+            var ret = new List<ILogicOutputChanged>();
+            if (!_moving || _direction != 0)
+            {
+                ret.Add(new LogicOutputChanged(_moveOutput, 1));
+                ret.Add(new LogicOutputChanged(_isMovingOutput, true));
+            }
+
+            _moving = true;
+            _direction = 0;
+            return ret;
+        }
+
+        private IList<ILogicOutputChanged> Stop()
+        {
+            var ret = new List<ILogicOutputChanged>();
+            if (_moving)
+            {
+                var stopValue = _direction == 0 ? true : false;
+                ret.Add(new LogicOutputChanged(_stopOutput, stopValue));
+                ret.Add(new LogicOutputChanged(_isMovingOutput, false));
+            }
+
+            _moving = false;
+            return ret;
+        }
+
+        public Task StopAsync(CancellationToken token = new CancellationToken())
+        {
+            var values = Stop();
+            return DispatchValues(values);
+        }
+
+
+        public Task MoveUpAsync(CancellationToken token = new CancellationToken())
+        {
+            var values = MoveUp();
+            return DispatchValues(values);
+        }
+
+        public Task MoveDownAsync(CancellationToken token = new CancellationToken())
+        {
+            var values = MoveDown();
+            return DispatchValues(values);
+        }
+
+        public Task MoveAbsoluteAsync(int pos, CancellationToken token = new CancellationToken())
+        {
+            var values = MoveAbsolute(pos);
+            return DispatchValues(values);
+        }
+
+        private async Task DispatchValues(IList<ILogicOutputChanged> list)
+        {
+            foreach (var l in list)
+            {
+                await Context.Dispatcher.DispatchValue(l.Instance,
+                    new DispatchValue(l.Instance.Id, DispatchableType.RuleInstance, l.Value, DateTime.Now,
+                        DispatchValueSource.Read));
+            }
         }
     }
 }
