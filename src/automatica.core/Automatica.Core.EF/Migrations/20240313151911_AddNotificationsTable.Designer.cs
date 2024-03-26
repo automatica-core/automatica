05ca720bbc3b5832657f567879e2b1f36b92a6e6
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2024-03-26 20:28:09 +0100

    Revert "remove last migration"
    This reverts commit 39e9d7ce0ab1dea4570d94fbe3c4935d96e78f55.

--- src/automatica.core/Automatica.Core.EF/Migrations/20240313151911_AddNotificationsTable.Designer.cs
+++ src/automatica.core/Automatica.Core.EF/Migrations/20240313151911_AddNotificationsTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Automatica.Core.EF.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Automatica.Core.EF.Migrations
 {
     [DbContext(typeof(AutomaticaContext))]
-    partial class AutomaticaContextModelSnapshot : ModelSnapshot
+    [Migration("20240313151911_AddNotificationsTable")]
+    partial class AddNotificationsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.1");
