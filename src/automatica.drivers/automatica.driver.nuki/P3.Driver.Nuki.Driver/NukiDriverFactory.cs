9b5e51bb2599c0ca4008e663b72d80534c8b05d4
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2024-12-29 10:58:55 +0100

    update uids

--- src/automatica.drivers/automatica.driver.nuki/P3.Driver.Nuki.Driver/NukiDriverFactory.cs
+++ src/automatica.drivers/automatica.driver.nuki/P3.Driver.Nuki.Driver/NukiDriverFactory.cs
@@ -33,16 +33,16 @@ namespace P3.Driver.Nuki.Driver
             factory.CreateNodeTemplate(DriverGuid, "NUKI.NAME", "NUKI.DESCRIPTION", "NUKI", 
                 GuidTemplateTypeAttribute.GetFromEnum(InterfaceTypeEnum.Virtual), DriverGuid, false, false, true, false, true, NodeDataType.NoAttribute, 1, false);
 
-            factory.CreatePropertyTemplate(new Guid("07453b89-d9df-450c-9208-7b08d317e2c0"), "NUKI.POLL_INTERVAL.NAME", "NUKI.POLL_INTERVAL.DESCRIPTION", "poll",
+            factory.CreatePropertyTemplate(new Guid("243b2e63-21b4-40e0-8e4d-295bb0a4894c"), "NUKI.POLL_INTERVAL.NAME", "NUKI.POLL_INTERVAL.DESCRIPTION", "poll",
                 PropertyTemplateType.Range, DriverGuid, "COMMON.CATEGORY.ADDRESS", true, false, PropertyHelper.CreateRangeMetaString(TimeSpan.FromSeconds(1).TotalSeconds, TimeSpan.FromHours(2).TotalSeconds), TimeSpan.FromMinutes(2).TotalSeconds, 1, 1);
 
-            factory.CreatePropertyTemplate(new Guid("07453b89-d9df-450c-9208-7b08d317e2c0"), "NUKI.IP_ADDRESS.NAME", "NUKI.IP_ADDRESS.DESCRIPTION", "ip",
+            factory.CreatePropertyTemplate(new Guid("d6d2f651-70e5-4bbc-9a7a-baf5576cfbae"), "NUKI.IP_ADDRESS.NAME", "NUKI.IP_ADDRESS.DESCRIPTION", "ip",
                 PropertyTemplateType.Ip, DriverGuid, "COMMON.CATEGORY.ADDRESS", true, false, null, null, 1, 2);
 
-            factory.CreatePropertyTemplate(new Guid("07453b89-d9df-450c-9208-7b08d317e2c0"), "NUKI.PORT.NAME", "NUKI.PORT.DESCRIPTION", "port",
+            factory.CreatePropertyTemplate(new Guid("d24e0269-305d-47c0-b10c-1d13397e4e94"), "NUKI.PORT.NAME", "NUKI.PORT.DESCRIPTION", "port",
                 PropertyTemplateType.Integer, DriverGuid, "COMMON.CATEGORY.ADDRESS", true, false, null, 8080, 1, 3);
 
-            factory.CreatePropertyTemplate(new Guid("07453b89-d9df-450c-9208-7b08d317e2c0"), "NUKI.ACCESS_TOKEN.NAME", "NUKI.ACCESS_TOKEN.DESCRIPTION", "token",
+            factory.CreatePropertyTemplate(new Guid("2260ec26-9f88-4c90-91f8-072a852c92a3"), "NUKI.ACCESS_TOKEN.NAME", "NUKI.ACCESS_TOKEN.DESCRIPTION", "token",
                 PropertyTemplateType.Password, DriverGuid, "COMMON.CATEGORY.ADDRESS", true, false, null, null, 1, 4);
 
         }
