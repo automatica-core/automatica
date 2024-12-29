2605fe5e67fabeb4b5d24a2d34b1819dcdbd6896
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2024-12-29 10:58:04 +0100

    add base nuki driver

---
+++ src/automatica.drivers/automatica.driver.nuki/P3.Driver.Nuki.Driver/NukiDriver.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Automatica.Core.Driver;
+using Microsoft.Extensions.Logging;
+using Timer = System.Threading.Timer;
+
+namespace P3.Driver.Nuki.Driver
+{
+    internal class NukiDriver : DriverNoneAttributeBase
+    {
+        private Timer _timer;
+      
+        private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
+        private readonly ILogger _logger;
+        private int _pollTime;
+
+        public NukiDriver(IDriverContext driverContext) : base(driverContext)
+        {
+            _logger = driverContext.Logger;
+        }
+
+        public override Task<bool> Init(CancellationToken token = default)
+        {
+            var pollTime = GetPropertyValueInt("poll");
+            _pollTime = pollTime;
+            return base.Init(token);
+        }
+
+        private async void TimeElapsed(object state)
+        {
+            try
+            {
+                if (await _semaphore.WaitAsync(TimeSpan.FromSeconds(1)))
+                {
+                    await ReadValues();
+                }
+            }
+            catch (Exception ex)
+            {
+                DriverContext.Logger.LogError(ex, "Error read values...");
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+
+        public override async Task<bool> Start(CancellationToken token = default)
+        {
+            _timer = new Timer(TimeElapsed, this, _pollTime * 1000, _pollTime * 1000);
+
+            _logger.LogInformation($"Start polling every {_pollTime}s");
+            await ReadValues(token);
+
+            return await base.Start(token);
+        }
+
+      
+        protected override async Task<bool> Read(IReadContext readContext, CancellationToken token = new CancellationToken())
+        {
+            await ReadValues(token);
+            return true;
+        }
+
+        private async Task ReadValues(CancellationToken token = default)
+        {
+            _logger.LogDebug($"Poll values...");
+
+           
+        }
+
+        public override Task<bool> Stop(CancellationToken token = default)
+        {
+            _timer.Dispose();
+            return base.Stop(token);
+        }
+
+        public override IDriverNode CreateDriverNode(IDriverContext ctx)
+        {
+          
+
+            return null;
+        }
+    }
+}
---
+++ src/automatica.drivers/automatica.driver.nuki/P3.Driver.Nuki.Driver/NukiDriverFactory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Automatica.Core.Base.Templates;
+using Automatica.Core.Driver;
+using Automatica.Core.EF.Models;
+using NodeDataType = Automatica.Core.Base.Templates.NodeDataType;
+
+namespace P3.Driver.Nuki.Driver
+{
+    public class NukiDriverFactory : DriverFactory
+    {
+        public override string DriverName => "Nuki";
+
+        public override Guid DriverGuid => new Guid("da739d07-56a6-4cd3-a3dc-ac7c5d2d4fe5");
+
+        public override Version DriverVersion => new Version(1, 0, 0, 0);
+
+        public override string ImageName => "automaticacore/plugin-p3.driver.nuki";
+
+        public override bool InDevelopmentMode => false;
+        public override IDriver CreateDriver(IDriverContext config)
+        {
+            return new NukiDriver(config);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="factory"></param>9
+        public override void InitNodeTemplates(INodeTemplateFactory factory)
+        {
+            factory.CreateInterfaceType(DriverGuid, "NUKI.NAME", "NUKI.DESCRIPTION", int.MaxValue, 1, true);
+
+            factory.CreateNodeTemplate(DriverGuid, "NUKI.NAME", "NUKI.DESCRIPTION", "NUKI", 
+                GuidTemplateTypeAttribute.GetFromEnum(InterfaceTypeEnum.Virtual), DriverGuid, false, false, true, false, true, NodeDataType.NoAttribute, 1, false);
+
+            factory.CreatePropertyTemplate(new Guid("07453b89-d9df-450c-9208-7b08d317e2c0"), "NUKI.POLL_INTERVAL.NAME", "NUKI.POLL_INTERVAL.DESCRIPTION", "poll",
+                PropertyTemplateType.Range, DriverGuid, "COMMON.CATEGORY.ADDRESS", true, false, PropertyHelper.CreateRangeMetaString(TimeSpan.FromSeconds(1).TotalSeconds, TimeSpan.FromHours(2).TotalSeconds), TimeSpan.FromMinutes(2).TotalSeconds, 1, 1);
+
+            factory.CreatePropertyTemplate(new Guid("07453b89-d9df-450c-9208-7b08d317e2c0"), "NUKI.IP_ADDRESS.NAME", "NUKI.IP_ADDRESS.DESCRIPTION", "ip",
+                PropertyTemplateType.Ip, DriverGuid, "COMMON.CATEGORY.ADDRESS", true, false, null, null, 1, 2);
+
+            factory.CreatePropertyTemplate(new Guid("07453b89-d9df-450c-9208-7b08d317e2c0"), "NUKI.PORT.NAME", "NUKI.PORT.DESCRIPTION", "port",
+                PropertyTemplateType.Integer, DriverGuid, "COMMON.CATEGORY.ADDRESS", true, false, null, 8080, 1, 3);
+
+            factory.CreatePropertyTemplate(new Guid("07453b89-d9df-450c-9208-7b08d317e2c0"), "NUKI.ACCESS_TOKEN.NAME", "NUKI.ACCESS_TOKEN.DESCRIPTION", "token",
+                PropertyTemplateType.Password, DriverGuid, "COMMON.CATEGORY.ADDRESS", true, false, null, null, 1, 4);
+
+        }
+
+    }
+}
